3a33df0aefd5ad50aa939db266c3edbd0edf8621
Tim Plunkett <git@plnktt.com>
2014-03-14 16:13:38 -0700

    WP-3124: Each change to the visibility field should only trigger one request.

--- js/gsb_field_authors.js
+++ js/gsb_field_authors.js
@@ -5,31 +5,23 @@
  */
 Drupal.behaviors.gsbFieldAuthors = {
   attach: function (context) {
-    $(context).find('.gsb-field-authors-visibility').each(function () {
-      new Drupal.GsbFieldAuthorsSelection($(this));
+    $(context).find('.gsb-field-authors-visibility').once(function () {
+      var $element = $(this);
+      // Create a new Drupal.ajax object that responds to the
+      // gsb_field_authors_change event.
+      var ajax = new Drupal.ajax(false, $element, {
+        event: 'gsb_field_authors_change',
+        url: Drupal.settings.basePath + 'gsb-field-authors/visibility-update/' + $element.data('gsb-field-authors-entity-type') + '/' + $element.data('gsb-field-authors-entity-id')
+      });
+
+      // When the element is changed, store the new value in the ajax data and
+      // trigger the event.
+      $element.change(function () {
+        ajax.options.data.profile_visibility_value = $(this).val();
+        $(ajax.element).trigger('gsb_field_authors_change');
+      });
     });
   }
-};
-
-/**
- * Creates a new selection object.
- */
-Drupal.GsbFieldAuthorsSelection = function ($element) {
-  var self = {
-    $element: $element,
-    entityType: $element.data('gsb-field-authors-entity-type'),
-    entityId: $element.data('gsb-field-authors-entity-id')
-  };
-
-  self.$element.change(function () {
-    var ajax = new Drupal.ajax(false, self.$element, {
-      event: 'gsb_field_authors_change',
-      url: Drupal.settings.basePath + 'gsb-field-authors/visibility-update/' + self.entityType + '/' + self.entityId + '/' + self.$element.val()
-    });
-    $(ajax.element).trigger('gsb_field_authors_change');
-  });
-
-  $.extend(this, self);
 };
 
 /**
